c44c93799a9f507c3d5a2c868a73f06c9b3cc33f
S-Eddine <alaoui.salah@hotmail.com>
2022-10-03 20:15:32 +0200

    Init project + Add next + create Pages + Add global style + add modules

---
+++ components/Footer.tsx
@@ -0,0 +1,5 @@
+function Footer(){
+    return <footer>Copyright Netx 2022</footer>
+}
+
+export default Footer;
---
+++ pages/ninjas/index.tsx
@@ -0,0 +1,5 @@
+function NinjasList(){
+    return <h1>This is ninjas lsi</h1>
+}
+
+export default NinjasList;
